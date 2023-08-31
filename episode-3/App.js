d8ef20ad5003d55db2da3a5f4fdab121073a6e91
Joel <joel.panvel@gmail.com>
2023-08-31 12:43:02 +0530

    Completed the episode-3 of namaste-react

--- episode-3/App.js
+++ episode-3/App.js
@@ -11,10 +11,31 @@ import ReactDOM from "react-dom/client";
 
 // console.log(heading);
 
+// console.log(jsxHeading);
+
+//in order to write a heading component
+// const HeadingComponent = () => {
+// 	return <h1 className="component">This is a heading component</h1>;
+// };
+
+const element = <span>I am a Span </span>;
+
 //in order to write the code using jsx
-const jsxHeading = <h1 id="heading">Welcome to Namaste React from JSX!!</h1>;
-console.log(jsxHeading);
+const title = (
+	<h1 id="heading" className="head">
+		{element}
+		Welcome to Namaste React from JSX!!
+	</h1>
+);
+
+const HeadingComponent = () => (
+	<div className="container">
+		{title}
+		{/* <h2>{value}</h2> */}
+		<h1 className="component">This is a Heading component</h1>
+	</div>
+);
 
 const rootFile = ReactDOM.createRoot(document.getElementById("rootFile"));
 
-rootFile.render(jsxHeading);
+rootFile.render(<HeadingComponent />);
